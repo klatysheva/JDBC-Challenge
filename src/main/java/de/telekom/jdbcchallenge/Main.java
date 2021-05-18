2207741df7efe02c786cd90ce2f492d02780e339
Kseniia Latysheva <latysheva.ks@yandex.ru>
2021-05-18 23:07:51 +0300

    Connections. V1.2

--- src/main/java/de/telekom/jdbcchallenge/Main.java
+++ src/main/java/de/telekom/jdbcchallenge/Main.java
@@ -4,33 +4,49 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tst");
-
         final String DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String URL = "jdbc:mysql://localhost:3306/seadb?user=seauser&password=seapass";
+
         try {
             Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        final String URL = "jdbc:mysql://localhost:3306/seadb?user=seauser&password=seapass";
+
         try {
             Connection connection = DriverManager.getConnection(URL);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery( "select * from persons");
-            while(resultSet.next()) {
-                System.out.println(resultSet.getInt(1));
-                System.out.println(resultSet.getString(2));
-                System.out.println(resultSet.getString(3));
-                System.out.println(resultSet.getString(4));
+            ResultSet selectAll = statement.executeQuery( "select * from persons");
+            while(selectAll.next()) {
+                System.out.println(selectAll.getInt(1));
+                System.out.println(selectAll.getInt(2));
+                System.out.println(selectAll.getString(3));
+                System.out.println(selectAll.getString(4));
             }
-            resultSet.close();
+
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT into persons (ID, SALUTATION, NAME, SURNAME) values (?,?,?,?)");
+            preparedStatement.setInt(1, 8);
+            preparedStatement.setInt(2, 0);
+            preparedStatement.setString(3, "Name");
+            preparedStatement.setString(4, "Surname");
+            preparedStatement.execute();
+            preparedStatement.close();
+
+            selectAll = statement.executeQuery( "select * from persons");
+            while(selectAll.next()) {
+                System.out.println(selectAll.getInt(1));
+                System.out.println(selectAll.getInt(2));
+                System.out.println(selectAll.getString(3));
+                System.out.println(selectAll.getString(4));
+            }
+
+            selectAll.close();
             statement.close();
             connection.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
 
     }
 }
